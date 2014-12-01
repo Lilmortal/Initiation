1311dcdffccae4288ea974cb8dbd3671ed50d9ad
Zeanith <janidhu_github@hotmail.com>
2014-12-01 15:45:50 +1300

    abstractMircleMember Comment
    updated abstractMircleMember comment

--- Level 1/abstractMircleMember.java
+++ Level 1/abstractMircleMember.java
@@ -1,11 +1,12 @@
 /*
 	Declare an abstract method. Method must be titled "yourFirstName()". The method can do whatever you want,
-	just make sure it works. Implement it in MircleInitiation.java
+	just make sure it works. Implement it in MircleMemberInitiation.java
 
 */
 abstract class abstractMircleMember {
  
 	abstract void Janidhu();
 	abstract void Jason();
+	abstract void Jordan();
 
 }
