aa4d68521ccf0d7f34c748d7e1af29fea7bd7d38
Zeanith <janidhu_github@hotmail.com>
2014-12-01 15:11:04 +1300

    Simple First Level
    Level 1 complete

---
+++ Level 1/MircleInitiation.java
@@ -0,0 +1,7 @@
+public class MircleInitiation {
+	public static void main(String[] args) {
+		MircleMemberImplementation mirc = new MircleMemberImplementation();
+		mirc.Janidhu();
+		mirc.Jason();
+	}
+}
---
+++ Level 1/MircleMemberImplementation.java
@@ -0,0 +1,12 @@
+
+
+public class MircleMemberImplementation extends abstractMircleMember {
+	
+	void Janidhu() {
+		System.out.println("Hi, I'm Janidhu and I'm a lot cooler than Dennis");
+	}
+
+	void Jason() {
+		System.out.println("Hi, I'm Jason and I think Microsoft Word can be used as a Java text editor");
+	}
+}
